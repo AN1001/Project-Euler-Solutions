cc8c4e512ca3b1fec47816e8106fe49e418531eb
Arnav Nagpure <81749717+AN1001@users.noreply.github.com>
2024-08-19 21:17:49 +0100

    Update 67-MaximumPathSumII.cpp

--- 0-100/67-MaximumPathSumII.cpp
+++ 0-100/67-MaximumPathSumII.cpp
@@ -2,19 +2,6 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <vector>
-
-std::vector<std::string> split (const std::string &s, char delim) {
-    std::vector<std::string> result;
-    std::stringstream ss (s);
-    std::string item;
-
-    while (getline (ss, item, delim)) {
-        result.push_back (item);
-    }
-
-    return result;
-}
 
 int main() {
         std::cout << std::endl;
@@ -24,12 +11,13 @@ int main() {
 
         if(!data){
                 std::cout << "No such file found" << std::endl;
-        }
+        }      
 
-        int number_rows = 100;
+        //Should be using 'constexpr' instead of 'const', but on old compiler :( so not supported
+        const int number_rows = 100;
 
-        int previous_row[100] = { 0 };
-        int current_row[100] = { 0 };
+        int previous_row[number_rows] = { 0 };
+        int current_row[number_rows] = { 0 };
         int width = 0;
         while (getline (data, row)) {
                 int* p = current_row;
@@ -50,14 +38,10 @@ int main() {
                         index++;
                 }
 
-                for(int i=0; i<number_rows - 1; i++){
-                //      std::cout << previous_row[i] << "->";
-                        current_row[i] += previous_row[i];
-                        current_row[i+1] += previous_row[i];
-                //      std::cout << current_row[i] << std::endl;
+                current_row[0] += previous_row[0];
+                for(int i=1; i<number_rows; i++){
+                        current_row[i] += std::max(previous_row[i], previous_row[i-1]);
                 }
-
-                std::cout << std::endl;
 
         }
         data.close();
@@ -71,3 +55,4 @@ int main() {
         std::cout << std::endl << max;
 
 }
+//Output: 7273
